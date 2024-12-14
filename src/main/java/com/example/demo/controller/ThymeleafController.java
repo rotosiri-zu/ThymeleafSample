2c21df9f1d53c319e8e601f4620222110f1c651b
rotosiri-zu <hongjitufang25@gmail.com>
2024-12-14 15:16:07 +0900

    コントローラへの追記(ModeladdAttribute)

--- src/main/java/com/example/demo/controller/ThymeleafController.java
+++ src/main/java/com/example/demo/controller/ThymeleafController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ThymeleafController {
 	@GetMapping("show")
 	public String showView(Model model) {
+		// 「Model」にデータを格納する
+		model.addAttribute("name", "太郎");
 		// 戻り値は「ビュー名」を返す
 		return "main";
 	}
